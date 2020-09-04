69e87ccbb7fb7d853b98015add4a2ccc77735181
maria alvarez <mar.alvareg@gmail.com>
2020-09-04 12:43:49 -0400

    querys

--- prueba.sql
+++ prueba.sql
@@ -5,8 +5,6 @@ CREATE TABLE clientes(id SERIAL PRIMARY KEY, nombre VARCHAR(50), rut VARCHAR(10)
 
 CREATE TABLE categorias(id SERIAL PRIMARY KEY, nombre VARCHAR(50) NOT NULL UNIQUE, descripcion VARCHAR(100) DEFAULT 'sin descripcion');
 
--- FUNCIONA CON SQL NO CON POSTGRE 
--- CREATE TABLE facturas(id SERIAL PRIMARY KEY, fecha_factura DATE NOT NULL DEFAULT NOW()::DATE, subtotal FLOAT NOT NULL, iva AS subtotal*0.19, precio_total AS subtotal+iva);
 CREATE TABLE facturas(id SERIAL PRIMARY KEY, fecha_factura DATE NOT NULL DEFAULT NOW()::DATE, subtotal FLOAT, iva FLOAT, precio_total FLOAT);
 
 CREATE TABLE productos(id SERIAL PRIMARY KEY, nombre VARCHAR(50), descripcion VARCHAR(100) DEFAULT 'sin descripcion', valor_unitario FLOAT);
@@ -25,16 +23,16 @@ INSERT INTO productos(nombre, valor_unitario) VALUES ('manzana', 500), ('lechuga
 INSERT INTO categorias(nombre) VALUES ('frutas'), ('verduras'),('legumbres');
 
 --10 facturas (para crear los ids)
-INSERT INTO facturas(subtotal, iva, precio_total) VALUES(2250);
-INSERT INTO facturas(subtotal, iva, precio_total) VALUES(2300);
-INSERT INTO facturas(subtotal, iva, precio_total) VALUES(2050);
-INSERT INTO facturas(subtotal, iva, precio_total) VALUES(2400);
-INSERT INTO facturas(subtotal, iva, precio_total) VALUES(2800);
-INSERT INTO facturas(subtotal, iva, precio_total) VALUES(500);
-INSERT INTO facturas(subtotal, iva, precio_total) VALUES(2500);
-INSERT INTO facturas(subtotal, iva, precio_total) VALUES(2500);
-INSERT INTO facturas(subtotal, iva, precio_total) VALUES(3850);
-INSERT INTO facturas(subtotal, iva, precio_total) VALUES(400);
+INSERT INTO facturas(subtotal) VALUES(0);
+INSERT INTO facturas(subtotal) VALUES(0);
+INSERT INTO facturas(subtotal) VALUES(0);
+INSERT INTO facturas(subtotal) VALUES(0);
+INSERT INTO facturas(subtotal) VALUES(0);
+INSERT INTO facturas(subtotal) VALUES(0);
+INSERT INTO facturas(subtotal) VALUES(0);
+INSERT INTO facturas(subtotal) VALUES(0);
+INSERT INTO facturas(subtotal) VALUES(0);
+INSERT INTO facturas(subtotal) VALUES(0);
 
 --2 para el cliente 1, con 2 y 3 productos distintos
 --3 para el cliente 2, con 3, 2 y 3 productos distintos
@@ -42,3 +40,30 @@ INSERT INTO facturas(subtotal, iva, precio_total) VALUES(400);
 --4 para el cliente 4, con 2, 3, 4 y 1 producto distintos
 INSERT INTO clientes_facturas(cliente_id, factura_id) VALUES (1, 1), (1, 2), (2, 3), (2, 4), (2, 5), (3, 6), (4, 7), (4, 8), (4, 9), (4, 10);
 INSERT INTO listado_productos(factura_id, producto_id, cantidad) VALUES (1,4,2), (1,2,1),(2,2,1), (2,3,2), (2,4,1), (3,1,1), (3,3,2),(3,4,1), (4, 5, 2),(4,1,2), (5,6,2), (5,4,1), (5,2,1), (6,7,1), (7,8,2), (7,1,1), (8,7,1), (8,2,2), (8,1,1), (9,3,2), (9,4,2), (9,4,1), (9,3,2), (10,3,1);
+
+--revisar los subtotales por factura
+SELECT sum(valor_unitario*cantidad) AS subtotal, factura_id FROM productos p JOIN listado_productos l ON p.id=l.producto_id GROUP BY (factura_id);
+--set subtotal
+UPDATE facturas SET subtotal=(SELECT SUM(valor_unitario*cantidad) FROM productos p JOIN listado_productos l ON p.id=l.producto_id WHERE factura_id=1) WHERE id=1;
+UPDATE facturas SET subtotal=(SELECT SUM(valor_unitario*cantidad) FROM productos p JOIN listado_productos l ON p.id=l.producto_id WHERE factura_id=2) WHERE id=2;
+UPDATE facturas SET subtotal=(SELECT SUM(valor_unitario*cantidad) FROM productos p JOIN listado_productos l ON p.id=l.producto_id WHERE factura_id=3) WHERE id=3;
+UPDATE facturas SET subtotal=(SELECT SUM(valor_unitario*cantidad) FROM productos p JOIN listado_productos l ON p.id=l.producto_id WHERE factura_id=4) WHERE id=4;
+UPDATE facturas SET subtotal=(SELECT SUM(valor_unitario*cantidad) FROM productos p JOIN listado_productos l ON p.id=l.producto_id WHERE factura_id=5) WHERE id=5;
+UPDATE facturas SET subtotal=(SELECT SUM(valor_unitario*cantidad) FROM productos p JOIN listado_productos l ON p.id=l.producto_id WHERE factura_id=6) WHERE id=6;
+UPDATE facturas SET subtotal=(SELECT SUM(valor_unitario*cantidad) FROM productos p JOIN listado_productos l ON p.id=l.producto_id WHERE factura_id=7) WHERE id=7;
+UPDATE facturas SET subtotal=(SELECT SUM(valor_unitario*cantidad) FROM productos p JOIN listado_productos l ON p.id=l.producto_id WHERE factura_id=8) WHERE id=8;
+UPDATE facturas SET subtotal=(SELECT SUM(valor_unitario*cantidad) FROM productos p JOIN listado_productos l ON p.id=l.producto_id WHERE factura_id=9) WHERE id=9;
+UPDATE facturas SET subtotal=(SELECT SUM(valor_unitario*cantidad) FROM productos p JOIN listado_productos l ON p.id=l.producto_id WHERE factura_id=10) WHERE id=10;
+--set los ivas
+UPDATE facturas SET iva=subtotal*0.19;
+--set precio total
+UPDATE facturas SET precio_total=subtotal*1.19;
+
+--¿Que cliente realizó la compra más cara?
+SELECT nombre FROM clientes JOIN clientes_facturas ON clientes.id=clientes_facturas.cliente_id JOIN facturas ON facturas.id=clientes_facturas.factura_id ORDER BY(precio_total) DESC LIMIT(1);
+
+--¿Que cliente pagó sobre 100 de monto?
+SELECT nombre FROM clientes JOIN clientes_facturas ON clientes.id=clientes_facturas.cliente_id JOIN facturas ON facturas.id=clientes_facturas.factura_id WHERE precio_total > 100 GROUP BY(nombre);
+
+--¿Cuantos clientes han comprado el producto 6
+SELECT count(nombre) FROM clientes JOIN clientes_facturas ON clientes.id=clientes_facturas.cliente_id JOIN facturas ON facturas.id=clientes_facturas.factura_id JOIN listado_productos ON facturas.id=listado_productos.factura_id WHERE producto_id=6;
